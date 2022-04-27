0454ec0e04d4803e52acb3a5516cfd56a3bc1a7b
iamminho <minho100227@kakao.com>
2022-04-27 22:06:28 +0900

    tailwindCSS setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next'
 
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black-1000">hello</h1>
+    </div>
+  );
 };
 
 export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./pages/**/*.tsx",
+            "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
